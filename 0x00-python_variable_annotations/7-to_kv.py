8323c03fbdb56cc80973473841c4a37993cc8be7
omaryahia4 <omar.yahia4444@gmail.com>
2022-01-31 12:35:21 +0100

    documentation

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-""""""
+"""Module"""
+from typing import Tuple, Union
 
 
-import typing
-from typing import Tuple
-
-x = Tuple[str, float]
-def to_kv(k: str, v: typing.Union[int, float])-> tuple:
+def to_kv(k: str, v: Union[int, float]) -> tuple:
+    """Function that that takes a string
+    k and an int OR float v as arguments and returns a tuple"""
+    x = Tuple[str, float]
     x = (k, v*v)
     return x
