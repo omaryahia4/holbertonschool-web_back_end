712ec0e08a5c351d7ee065414914be06e9d3e1dd
omaryahia4 <omar.yahia4444@gmail.com>
2022-01-31 09:38:12 +0100

    documentation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+""""""
 
 def add(a: float, b: float) -> float:
     """Function that return sum of two floats"""
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""""""
+
+def concat(str1: str, str2: str) -> str:
+    """Function that concatenates two strings"""
+    return str1 + str2
