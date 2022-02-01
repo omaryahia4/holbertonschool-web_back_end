81b090457d9e33b31604ff6f612176f8a2a79491
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-01 15:01:19 +0100

    function's delays

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Execution time"""
+
+import typing
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """Function that returns list of function's delays"""
+    l: typing.List[float] = []
+    for _ in range(n):
+        task = await task_wait_random(max_delay)
+        l.append(task)
+    return sorted(l)
