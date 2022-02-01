0f235029e50af6c7b1476b01f4ac0a37a4d9ea3c
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-01 11:05:25 +0100

    annotations

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """Function that returns the waiting time
     before executing a function"""
     time = random.uniform(0, max_delay)
