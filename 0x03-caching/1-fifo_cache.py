339e8262ebda595b81b83d225f9c7fb7eeca5013
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-04 16:56:31 +0100

    FIFO caching

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""FIFO caching"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFO class"""
+    def __init__(self):
+        """init function"""
+        super().__init__()
+        self.called = ""
+
+    def put(self, key, item):
+        """Function that add new items to dictionary"""
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            self.cache_data[key] = self.cache_data.pop(self.called)
+            first = list(self.cache_data)[0]
+            self.cache_data.pop(first)
+            print("DISCARD: {}".format(first, end=""))
+        else:
+            pass
+
+    def get(self, key):
+        """Function that gets data from dicitonary"""
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
