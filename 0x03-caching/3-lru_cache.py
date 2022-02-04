3f2541163730c941969ce9d3b6c01e5734ad1410
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-04 17:04:04 +0100

    LRU Caching

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""LIFO caching"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LIFO class"""
+    def __init__(self):
+        """init function"""
+        super().__init__()
+        self.called = ""
+
+    def get(self, key):
+        """Function that gets data from dicitonary"""
+        self.called = key
+        if key is None or key not in self.cache_data:
+            return None
+        if key in self.cache_data:
+            self.cache_data[key] = self.cache_data.pop(key)
+            return self.cache_data[key]
+        else:
+            return self.cache_data[self.called]
+
+    def put(self, key, item):
+        """Function that add new items to dictionary"""
+        if key and item:
+            self.cache_data[key] = item
+        if key in self.cache_data:
+            self.cache_data.pop(key)
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first = list(self.cache_data)[0]
+            self.cache_data.pop(first)
+            print("DISCARD: {}".format(first, end=""))
+
+        else:
+            pass
