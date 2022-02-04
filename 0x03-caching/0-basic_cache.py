906322d59f9ac9e7a56b199f2d8b14277d4d52b2
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-04 16:51:44 +0100

    Basic dictionary

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -6,13 +6,13 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """Basic cache class that inherits
     from BaseCaching class"""
-    cache_data = {}
 
     def put(self, key, item):
         """Function that add new items to dictionary"""
-        self.cache_data[key] = item
         if key or item is None:
             pass
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
         """Function that gets data from dicitonary"""
