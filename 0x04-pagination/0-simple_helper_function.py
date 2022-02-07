7817be3fdcae816b85147c892bfffa387edd5ad3
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-07 17:18:44 +0100

    Simple helper function

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -7,12 +7,5 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """function that returns a tuple of size two
     containing a start index and an end index"""
-    count = 0
-    page_content = page_size
-    li = []
-    for i in range(page, page_size):
-        if i > 11:
-            count += 10
-    page_content += count
-    li.extend([count, page_content])
-    return tuple(li)
+    size = page * page_size
+    return (size - page_size, size)
