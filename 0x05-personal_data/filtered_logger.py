5643469a9386cc76540f6438c60da9cbd6eb8fd1
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-11 08:28:46 +0100

    using regex to replace occurrences of certain field values

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -6,6 +6,8 @@ import re
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
+    """function called filter_datum that
+    returns the log message obfuscated"""
     for field in fields:
         message = re.sub(r'{}=.*?{}'.format(field, separator),
                          "{}={}{}".format(field,
