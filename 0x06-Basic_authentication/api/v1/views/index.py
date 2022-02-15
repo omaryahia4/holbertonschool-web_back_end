0d99936bd151152e146982f864b7abb0d7ac86fa
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-15 11:32:26 +0100

    add a new endpoint for forbidden

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -23,6 +23,15 @@ def unauthorized() -> str:
     abort(401)
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - raise a 403 error
+    """
+    abort(403)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
