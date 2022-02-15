951aa672ebc154c90384427b63f49d106057af46
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-15 11:33:00 +0100

    Add a new error handler for 403 error

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -28,6 +28,13 @@ def not_authorized(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
