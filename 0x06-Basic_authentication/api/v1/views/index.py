bf9ad1af851a18ce35aa406f525565a9878a4271
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-15 11:12:10 +0100

    add a new endpoint

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -3,7 +3,7 @@
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
-
+from api.v1.app import not_authorized
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
@@ -13,6 +13,14 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - raise a 401 error
+    """
+    abort(401)
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
