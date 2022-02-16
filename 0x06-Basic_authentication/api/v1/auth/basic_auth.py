db4746b99abefacd3f96319d3ad9967ea15f1372
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-16 10:33:51 +0100

    Basic - Base64 decode

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Module of BasicAuth
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,18 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """method that returns the decoded
+        value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            in_bytes = base64.b64decode(base64_authorization_header)
+            return in_bytes.decode('utf-8')
+        except Exception:
+            return None
