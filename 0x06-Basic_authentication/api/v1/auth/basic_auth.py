7bc5ee2f59e2638bd4b096a17d893b48fa167c91
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-16 09:07:35 +0100

    basic authentication class

---
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module of BasicAuth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic authentication class
+    that inherits from Auth class"""
+    pass
