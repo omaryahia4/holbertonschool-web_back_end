2e8bcdb0202611f60dc2e486e5127ecde1046cfe
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-16 08:38:17 +0100

    add before_reuest handler

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -44,9 +44,9 @@ def before_request():
     """function that filters each request"""
     paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if auth is None:
-        pass
+        return
     if auth.require_auth(request.path, paths):
-        pass
+        return
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
