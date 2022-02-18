599561c091076520b332eb671c919f1c42de3a76
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-18 16:37:43 +0100

    logout route

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -28,3 +28,13 @@ def session_log():
         response = jsonify(user.to_json())
         response.set_cookie(getenv('SESSION_NAME'), session_id)
         return response
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def logout_session():
+    """logout route"""
+    from api.v1.app import auth
+    if auth.destroy_session(request) is False:
+        abort(404)
+    return jsonify({}), 200
