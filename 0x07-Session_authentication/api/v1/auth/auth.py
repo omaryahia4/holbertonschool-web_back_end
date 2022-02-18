bab861966ccb56b094cd5529c62193ea1b6e4f6c
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-18 08:44:53 +0100

    session authentication

---
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" Module of Auth
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth():
+    """class to manage the API authentication.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """public method"""
+        special_character: str = '/'
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+        last_item = path[-1]
+        if last_item not in special_character:
+            path += '/'
+            if path in excluded_paths:
+                return False
+        else:
+            return True
+
+    def authorization_header(self, request=None) -> str:
+        """public method that returns
+        None - request will be the Flask request object"""
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        else:
+            return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """public method  that returns
+        None - request will be the Flask request object"""
+        return None
