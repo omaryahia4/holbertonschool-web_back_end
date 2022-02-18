6264e2398d5678ef6ad56afb11db0820233c111d
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-18 11:02:35 +0100

    another excluded path

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -54,15 +54,12 @@ def before_request():
         return
     if auth.require_auth(request.path, paths) is False:
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and\
+        auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
-    if auth.authorization_header(request)\
-            and auth.session_cookie(request) is None:
-        abort(401)
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
