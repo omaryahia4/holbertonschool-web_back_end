2491d5a6db6901e853bcf9fe69b4bb1ecef4746e
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-23 17:12:35 +0100

    register new user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
 """Auth model"""
 import bcrypt
+from user import User
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
     """method that takes in a password
      string arguments and returns bytes"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Method that Registers new users"""
+        try:
+            user = DB.find_user_by(email)
+            if user:
+                raise ValueError(f"User {user.email} already exists")
+        except Exception:
+            password = _hash_password(password)
+            user = self._db.add_user(email, password)
+            return user
