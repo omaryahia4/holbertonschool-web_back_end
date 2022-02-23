13ee62db53f3e87fb929445ff8f2083fe39f7afd
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-23 14:28:52 +0100

    create user

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Create user"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs):
+        """Method that finds user by
+        its credential"""
+        ele = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
+        for key in kwargs:
+            if key not in ele:
+                raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs):
+        """Method that updates user"""
+        ele = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if key not in ele:
+                raise ValueError
+            else:
+                setattr(user, key, value)
+        self._session.commit()
