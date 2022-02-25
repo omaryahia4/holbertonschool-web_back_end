c4834cdbc278ac56346e00b3d4382334555d0392
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-25 16:21:09 +0100

    reset password

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -61,5 +61,17 @@ def profile():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """reset password route"""
+    email = request.form.get('email')
+    if email:
+        AUTH.get_reset_password_token(email)
+        return jsonify({"email": "<user email>",
+                        "reset_token": "<reset token>"}), 200
+    else:
+        abort((403))
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
