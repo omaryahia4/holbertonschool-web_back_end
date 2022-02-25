345e1ea9e5ec16361e3ded399c5c61e73c760be2
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-25 11:24:25 +0100

    Credentials validation

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -3,6 +3,7 @@
 import bcrypt
 from user import User
 from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -21,10 +22,20 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         """Method that Registers new users"""
         try:
-            user = DB.find_user_by(email)
+            user = self._db.find_user_by(email=email)
             if user:
                 raise ValueError(f"User {user.email} already exists")
-        except Exception:
+        except NoResultFound:
             password = _hash_password(password)
             user = self._db.add_user(email, password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Credentials validation"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+            return False
+        except NoResultFound:
+            return False
