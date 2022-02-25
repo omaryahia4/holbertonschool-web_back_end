18978f95778c1b6525f04fea0afe93abdfa95d8a
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-25 15:31:25 +0100

    PROFILE route

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -49,6 +49,16 @@ def logout():
     else:
         abort(403)
 
+@app.route('/profile', methods=['GET'])
+def profile():
+    """Profile route"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
