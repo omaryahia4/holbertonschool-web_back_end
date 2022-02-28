a25d06ef231ba4c6c40bc1bf932e2015e84132f1
omaryahia4 <omar.yahia4444@gmail.com>
2022-02-28 07:42:03 +0100

    main function

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -82,9 +82,9 @@ def update_password():
     password = request.form.get('new_password')
     try:
         AUTH.update_password(token, password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
     except Exception:
         abort(403)
-    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
