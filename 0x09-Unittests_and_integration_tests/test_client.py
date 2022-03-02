84a5146b2809026f77cdcc13b37c0c91c83ff0d3
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-02 20:19:13 +0100

    test public repo_url

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 import unittest
 from client import GithubOrgClient
 from parameterized import parameterized
-from unittest.mock import patch
+from unittest.mock import PropertyMock, patch
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -18,6 +18,20 @@ class TestGithubOrgClient(unittest.TestCase):
         test_class = GithubOrgClient(url)
         self.assertEqual(test_class.org, payload.return_value)
         payload.assert_called_once()
+
+    def test_public_repos_url(self):
+        """Test that the result of _public_repos_url
+        is the expected one based on the mocked payload."""
+        with patch.object(GithubOrgClient,
+                          "org",
+                          new_callable=PropertyMock) as patched:
+            test_json = {"url": "linkedin",
+                         "repos_url": "http://google.com"}
+            patched.return_value = test_json
+            github_client = GithubOrgClient(test_json.get("url"))
+            response = github_client._public_repos_url
+            patched.assert_called_once()
+            self.assertEqual(response, test_json.get("repos_url"))
 
 
 if __name__ == '__main__':
