add922407a68e760b6eea8fbad0d4ac3314fac63
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-03 14:08:41 +0100

    Basic Flask app

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Babel module"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@app.route('/')
+def hello_world():
+    """home route"""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
