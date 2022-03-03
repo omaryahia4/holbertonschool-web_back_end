0ebdd568054b90dada97b41ad4a94e71d5616123
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-03 19:43:03 +0100

    Mock logging in

---
+++ 0x0A-i18n/5-app.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""Babel module"""
+from flask import Flask, request, render_template, g
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """Configuration class"""
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object(Config)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
+
+
+@app.route('/')
+def hello_world():
+    """home route"""
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """function that  determine the best
+    match with our supported languages"""
+    if request.args.get('locale'):
+        return request.args.get('locale')
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """function that returns a user dictionary"""
+    login_as = users[request.args.get('login_as')]
+    if login_as:
+        return login_as
+    else:
+        return None
+
+
+app.before_request
+
+
+def before_request():
+    """global"""
+    g.user = get_user()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
