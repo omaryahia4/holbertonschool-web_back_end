7f46b8d20c23ce785e4eedacfb62479642b8e228
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-03 15:04:54 +0100

    Parametrize templates

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Babel module"""
 from flask import Flask, request, render_template
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 app = Flask(__name__)
 babel = Babel(app)
