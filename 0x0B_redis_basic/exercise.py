bac287863d264fb43b47875f53537cd5932b9a07
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-15 15:43:51 +0100

    Incrementing values

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -32,7 +32,8 @@ class Cache():
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> None:
+    def get(self, key: str, fn: Optional[Callable] = None)\
+            -> Union[str, bytes, int, float]:
         """method that converts the data back
         to the desired format."""
         data = self._redis.get(key)
