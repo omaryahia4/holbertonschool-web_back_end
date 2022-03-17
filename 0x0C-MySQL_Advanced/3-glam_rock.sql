3177f46fb5ba48e6ddf249db138a1e84c988ad6d
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-17 12:14:38 +0100

    select bands by their lifespan

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,5 @@
 -- ranked by their longevity
 
 SELECT `band_name`, (IFNULL(split, 2022) - formed) AS lifespan
-FROM `metal_bands` WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+FROM `metal_bands` WHERE `style` LIKE '%Glam rock%'
+ORDER BY `lifespan` DESC;
