877ca302b605d3339e773661c843ef4f803a429f
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-21 16:55:52 +0100

    update mongo collection

---
+++ 0x0D-NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Change school topics"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school
+    document based on the name"""
+    return mongo_collection.update_many({'name': name},
+                                        {'$set': {'topics': topics}})
