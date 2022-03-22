10480e609907a66a7778776f7135b85a77a197a9
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-22 13:45:12 +0100

    fix

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -3,6 +3,7 @@
 
 
 def insert_school(mongo_collection, **kwargs):
-    """"""
+    """ function that inserts a new document
+    in a collection based on kwargs"""
     result = mongo_collection.insert_one(kwargs)
     return result and result.inserted_id
