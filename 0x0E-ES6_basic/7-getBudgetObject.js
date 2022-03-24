7e67cadf8566f4b25e3eb7dce6b94d211978d7fa
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-24 15:38:50 +0100

    shorthand syntax

--- 0x0E-ES6_basic/7-getBudgetObject.js
+++ 0x0E-ES6_basic/7-getBudgetObject.js
@@ -1,9 +1,5 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = {
-    income,
-    gdp,
-    capita
-  };
+  const budget = { income, gdp, capita };
 
   return budget;
 }
