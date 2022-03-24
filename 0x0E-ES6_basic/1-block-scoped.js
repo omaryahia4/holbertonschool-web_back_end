0ba1675a7c8314ee21acfe717055cf84f9598a84
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-24 11:54:10 +0100

    Block Scope

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -1,6 +1,6 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    let task2 = true;
+    const task = false;
+    const task2 = true;
 
     if (trueOrFalse) {
         let task = true;
