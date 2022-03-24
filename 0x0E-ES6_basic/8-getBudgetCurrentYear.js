a0d3853f7671c2e2bb7bb0d125830a38a42251f6
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-24 15:49:40 +0100

    computed property names

--- 0x0E-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x0E-ES6_basic/8-getBudgetCurrentYear.js
@@ -8,7 +8,7 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
 
   [`income-${getCurrentYear()}`]: income,
   [`gdp-${getCurrentYear()}`]: gdp,
-  [`capita-${getCurrentYear()}`]: capita
+  [`capita-${getCurrentYear()}`]: capita,
 }
   return budget;
 }
