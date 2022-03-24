d3df15dba569eab33eef159b47f4a5199c5f4652
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-24 20:40:45 +0100

    ES6 method properties

--- 0x0E-ES6_basic/9-getFullBudget.js
+++ 0x0E-ES6_basic/9-getFullBudget.js
@@ -1,4 +1,4 @@
-import getBudgetObject from './7-getBudgetObject';
+import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
@@ -12,5 +12,5 @@ export default function getFullBudgetObject(income, gdp, capita) {
     },
  };
 
-  return fullBudget;
+ return fullBudget;
 }
