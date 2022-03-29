90a47691418e8bbc8b76d1d6ce2e8db5b7fbe2f6
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-29 14:53:31 +0100

    eslit

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,11 +1,11 @@
 export default function getFullResponseFromAPI(success) {
-    const p = new Promise((resolve, reject) => {
-      if (success) {
-        resolve({ status: 200, body: 'Success' });
-      }
-      else {
-        reject(Error('The fake API is not working currently'));
-      }
-    });
-  return p;
+  const prom = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    }
+    else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+  return prom;
 }
