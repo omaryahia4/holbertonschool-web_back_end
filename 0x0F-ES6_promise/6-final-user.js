8c765ca807d714d959831097a6a6d251601453fa
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-30 13:54:22 +0100

    fix

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const arr = [];
   arr.push({ status: 'fulfilled', value: await signUpUser(firstName, lastName).then((response) => response) }, { status: 'rejected', value: await uploadPhoto(fileName).catch((err) => err) });
