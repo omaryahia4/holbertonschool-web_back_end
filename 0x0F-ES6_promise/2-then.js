47cd1c848b998ab78c66ac3dc849c75b8e3d12f2
omaryahia4 <omar.yahia4444@gmail.com>
2022-03-30 13:22:01 +0100

    fix

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Success' })
+  return promise.then(() => ({ status: 200, body: 'success' })
   ).catch(() => Error()
   ).finally(() => { console.log('Got a response from the API'); });
 }
