be374eee280fca291bdf107a7d4ceda949280e14
omaryahia4 <omar.yahia4444@gmail.com>
2022-04-01 08:06:48 +0100

    throw error

--- 0x0F-ES6_promise/8-try.js
+++ 0x0F-ES6_promise/8-try.js
@@ -1,8 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-  return new Promise((resolve, reject) => {
-    if (denominator > 0) {
-      resolve(numerator / denominator)
-}
-})
-
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+  return numerator / denominator;
 }
