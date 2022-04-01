5e493c9ef5903ea4236adaee565b87036948c5a9
omaryahia4 <omar.yahia4444@gmail.com>
2022-04-01 12:07:28 +0100

    abstract class

---
+++ 0x10-ES6_classes/5-building.js
@@ -0,0 +1,12 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && this.evacuationWarningMessage !== 'function') {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
