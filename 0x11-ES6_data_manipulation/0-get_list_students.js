557ca39fbbdf1ebf775a725fcf54c8933177aa16
omaryahia4 <omar.yahia4444@gmail.com>
2022-04-04 12:07:08 +0100

    array of objects

---
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+export default function getListStudents() {
+  const studentsList = [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }];
+  return studentsList;
+}
