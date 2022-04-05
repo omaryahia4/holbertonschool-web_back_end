3da59cb13dfc904eb01ce4d534278929d07f544b
omaryahia4 <omar.yahia4444@gmail.com>
2022-04-05 22:45:57 +0100

    Map

---
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const myMap = new Map();
+  myMap.set('Apples', 10);
+  myMap.set('Tomatoes', 10);
+  myMap.set('Pasta', 1);
+  myMap.set('Rice', 1);
+  myMap.set('Banana', 5);
+  return myMap;
+}
