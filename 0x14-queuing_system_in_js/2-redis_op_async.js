8743e7de3e13c5ab41178f5c0a6db891f0d25f60
omaryahia4 <omar.yahia4444@gmail.com>
2022-04-19 23:49:56 +0100

    promisify

---
+++ 0x14-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,31 @@
+import { createClient } from 'redis';
+
+const redis = require('redis');
+
+const client = createClient();
+const { promisify } = require('util');
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  const getAsync = promisify(client.get).bind(client);
+  const fooValue = await getAsync(schoolName);
+  console.log(fooValue);
+}
+
+async function asyncCall() {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+}
+asyncCall();
