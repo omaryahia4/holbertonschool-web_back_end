3bc3d9cbf2b49e472265a140f06bd66ec7fc2e4d
omaryahia4 <omar.yahia4444@gmail.com>
2022-04-20 15:24:04 +0100

    add data to job object

--- 0x14-queuing_system_in_js/6-job_creator.js
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -2,8 +2,8 @@ const kue = require('kue');
 
 const queue = kue.createQueue();
 const job = {
-  phoneNumber: '',
-  message: '',
+  phoneNumber: '232548695',
+  message: 'This is the code to verify your account',
 };
 
 const Newjob = queue.create('push_notification_code', job).save();
