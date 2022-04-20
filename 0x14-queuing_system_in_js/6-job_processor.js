b85ffdf663f09348f49ef13edea7ace83b08b402
omaryahia4 <omar.yahia4444@gmail.com>
2022-04-20 15:09:35 +0100

    Create the Job processor

---
+++ 0x14-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,10 @@
+const kue = require('kue');
+
+const queue = kue.createQueue();
+
+function sendNotification(phoneNumber, message) {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+}
+queue.process('push_notification_code', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message, done);
+});
