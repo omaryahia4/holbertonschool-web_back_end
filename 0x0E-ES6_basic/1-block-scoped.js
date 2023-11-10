6883dc46ade8d58640d15b73e70c0b0f0ed545be
omaryahia4 <omar.yahia4444@gmail.com>
2023-11-10 11:45:02 +0100

    try

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = false;
+    const task2 = true;
+  
+    if (trueOrFalse) {
+      /* eslint-disable */
+      const task = true;
+      const task2 = false;
+    }
+    
+  
+    return [task, task2];
   }
-
-  return [task, task2];
-}
