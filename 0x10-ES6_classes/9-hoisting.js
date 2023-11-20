911d06a0f0874e31fd1a3dd4c90626c09fe752ba
omaryahia4 <omar.yahia4444@gmail.com>
2023-11-20 12:23:26 +0100

    task 9

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -16,7 +16,6 @@ export class HolbertonClass {
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
