e4d6d112e5e36835827f194ffa510b7390fc463c
Omar <77851226+omaryahia4@users.noreply.github.com>
2024-03-08 12:11:54 +0100

    Update auth.py

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -9,21 +9,19 @@ class Auth():
     """class to manage the API authentication.
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """public method"""
-        special_character: str = '/'
-        if path is None:
-            return True
-        if excluded_paths is None or len(excluded_paths) == 0:
-            return True
-        if path in excluded_paths:
-            return False
-        last_item = path[-1]
-        if last_item not in special_character:
-            path += '/'
-            if path in excluded_paths:
-                return False
-        else:
-            return True
+    ''' require authorithation '''
+    if path is None or excluded_paths is None or not len(excluded_paths):
+        return True
+    if path[-1] != '/':
+        path += '/'
+    for i in excluded_paths:
+        if i.endswith('*'):
+            if path.startswith(i[:1]):
+                return True
+    if path in excluded_paths:
+        return False
+    else:
+        return True
 
     def authorization_header(self, request=None) -> str:
         """public method that returns
