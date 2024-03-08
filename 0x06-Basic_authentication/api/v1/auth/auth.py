8783586988680425be2e1020e889f932b16dc73e
Omar <77851226+omaryahia4@users.noreply.github.com>
2024-03-08 12:12:31 +0100

    Update auth.py

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,39 +1,39 @@
 #!/usr/bin/env python3
-""" Module of Auth
 """
+manages the API authentication
+"""
+from flask import Flask, request
 from typing import List, TypeVar
-from flask import request
 
 
 class Auth():
-    """class to manage the API authentication.
-    """
+    '''
+    a class that manages the API authentication
+    '''
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-    ''' require authorithation '''
-    if path is None or excluded_paths is None or not len(excluded_paths):
-        return True
-    if path[-1] != '/':
-        path += '/'
-    for i in excluded_paths:
-        if i.endswith('*'):
-            if path.startswith(i[:1]):
-                return True
-    if path in excluded_paths:
-        return False
-    else:
-        return True
+        ''' require authorithation '''
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for i in excluded_paths:
+            if i.endswith('*'):
+                if path.startswith(i[:1]):
+                    return True
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
-        """public method that returns
-        None - request will be the Flask request object"""
+        ''' authorization header '''
         if request is None:
             return None
-        if 'Authorization' not in request.headers:
+        if not request.headers.get("Authorization"):
             return None
-        else:
-            return request.headers['Authorization']
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """public method  that returns
-        None - request will be the Flask request object"""
+        ''' current user '''
         return None
