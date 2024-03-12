a10a947ae16e47142bf900bc685e885b27c07f7e
omaryahia4 <omar.yahia4444@gmail.com>
2024-03-12 09:50:53 +0100

    Advanced SQL

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,7 @@
--- SQL script that lists all bands with Glam rock as their main style,
--- ranked by their longevity
-
-
--- if the have splited we use split else we use current year 2023
-SELECT band_name, IFNULL(split, 2023) - IFNULL(formed, 0) AS lifespan
+-- Ranks country of origins by the number of non-unique fans
+--life span is diff between split and formed
+SELECT band_name,
+		TIMESTAMPDIFF(YEAR, `formed`, `split`) AS lifespan FROM metal_bands
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
