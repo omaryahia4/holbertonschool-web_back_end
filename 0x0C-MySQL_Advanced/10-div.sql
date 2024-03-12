f6d7d77fdefa701946ff4d77d09f2a58abda0a29
omaryahia4 <omar.yahia4444@gmail.com>
2024-03-12 09:52:36 +0100

    Advanced SQL

--- 0x0C-MySQL_Advanced/10-div.sql
+++ 0x0C-MySQL_Advanced/10-div.sql
@@ -1,3 +1,7 @@
+-- creates a function SafeDiv that divides (and returns)
+-- the first by the second number or returns 0 if the 
+-- second number is equal to 0.
+
 DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
