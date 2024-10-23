2cf4b358c9e48fc663aec69cfabbeef4ae21f017
omaryahia4 <omar.yahia4444@gmail.com>
2024-10-23 12:03:31 +0100

    i18n test

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python3
-"""Babel module"""
-from flask import Flask, request, render_template
+"""Route module for the API"""
+from flask import Flask, render_template, request
 from flask_babel import Babel
+
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
 class Config():
-    """Configuration class"""
+    """Config class"""
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
-Babel.default_locale = 'en'
-Babel.default_timezone = 'UTC'
+
+
+def get_locale():
+    """Get locale"""
+    if ('locale' in request.args and
+            request.args['locale'] in app.config['LANGUAGES']):
+        return request.args['locale']
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def hello_world():
-    """home route"""
+def index():
+    """Return index.html"""
     return render_template('4-index.html')
 
 
-@babel.localeselector
-def get_locale():
-    """function that  determine the best
-    match with our supported languages"""
-    if request.args.get('locale'):
-        return request.args.get('locale')
-    else:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+babel.init_app(app, locale_selector=get_locale)
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run()
