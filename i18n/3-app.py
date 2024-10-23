7a84e248bd5ef782063ff6b7808ec8433d345101
omaryahia4 <omar.yahia4444@gmail.com>
2024-10-23 12:00:41 +0100

    i18n test

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python3
-"""Babel module"""
-from flask import Flask, request, render_template
+"""Route module for the API"""
+from flask import Flask, render_template, request
 from flask_babel import Babel
+
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
 class Config():
-    """Configuration class"""
+    """Config class"""
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
-Babel.default_locale = 'en'
-Babel.default_timezone = 'UTC'
 
 
-@app.route('/')
-def hello_world():
-    """home route"""
-    return render_template('3-index.html')
 
-
-@babel.localeselector
 def get_locale():
-    """function that  determine the best
-    match with our supported languages"""
+    """Get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", debug=True)
+
+@app.route('/')
+def index():
+    """Return index.html"""
+    return render_template('3-index.html')
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+if __name__ == '__main__':
+    app.run()
