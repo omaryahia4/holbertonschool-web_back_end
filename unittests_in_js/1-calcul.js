dbc9c20e41b5680877a0a21f444bca736ef44c38
omaryahia4 <omar.yahia4444@gmail.com>
2025-12-02 15:56:00 +0100

    code fix

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -2,7 +2,7 @@ function calculateNumber(type, a, b) {
   if (type == "SUM") {
     return Math.round(a) + Math.round(b);
   } else if (type == "SUBTRACT") {
-    return Math.round(b) - Math.round(a);
+    return Math.round(a) - Math.round(b);
   } else if (type == "DIVIDE") {
     if (Math.round(b) == 0) {
       return "Error";
