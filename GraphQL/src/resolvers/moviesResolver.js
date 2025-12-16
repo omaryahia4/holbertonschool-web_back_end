4cac514fe33fd93b17885c179f6ac05ed78ab7e2
omaryahia4 <omar.yahia4444@gmail.com>
2025-12-16 11:46:03 +0100

    movie resolver

--- GraphQL/src/resolvers/moviesResolver.js
+++ GraphQL/src/resolvers/moviesResolver.js
@@ -2,12 +2,43 @@ import { moviesData } from '../data/moviesData.js';
 import { actorsData } from '../data/actorsData.js';
 
 export const movieResolvers = {
-  movie: ({ id }) => moviesData.getMovieById(id),
-  movies: () => moviesData.getAllMovies(),
-  createMovie: ({ input }) => moviesData.createMovie(input),
-  updateMovie: ({ id, input }) => moviesData.updateMovie(id, input),
-  deleteMovie: ({ id }) => moviesData.deleteMovie(id),
-  Movie: {
-    actors: (movie) => movie.actorIds.map(id => actorsData.getActorById(id))
+  movie: ({ id }) => {
+    const movie = moviesData.getMovieById(id);
+    if (!movie) return null;
+    return { 
+      ...movie, 
+      actors: movie.actorIds.map(aid => actorsData.getActorById(aid))
+    };
+  },
+
+  movies: () => {
+    return moviesData.getAllMovies().map(m => ({
+      ...m,
+      actors: m.actorIds.map(aid => actorsData.getActorById(aid))
+    }));
+  },
+
+  createMovie: ({ input }) => {
+    const newMovie = moviesData.createMovie(input);
+    return { 
+      ...newMovie, 
+      actors: newMovie.actorIds.map(aid => actorsData.getActorById(aid))
+    };
+  },
+
+  updateMovie: ({ id, input }) => {
+    const updated = moviesData.updateMovie(id, input);
+    return { 
+      ...updated, 
+      actors: updated.actorIds.map(aid => actorsData.getActorById(aid))
+    };
+  },
+
+  deleteMovie: ({ id }) => {
+    const deleted = moviesData.deleteMovie(id);
+    return { 
+      ...deleted, 
+      actors: deleted.actorIds.map(aid => actorsData.getActorById(aid))
+    };
   }
 };
