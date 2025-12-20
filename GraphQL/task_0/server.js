9d5cca620c989c8f81dd218c7e329a13dba3198e
omaryahia4 <omar.yahia4444@gmail.com>
2025-12-20 11:16:52 +0100

    task 0

---
+++ GraphQL/task_0/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { graphql, buildSchema } = require('graphql');
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const rootValue = {
+  hello: () => 'Hello GraphQL!',
+};
+
+app.post('/graphql', async (req, res) => {
+  const { query } = req.body;
+
+  try {
+    const result = await graphql({
+      schema,
+      source: query,
+      rootValue,
+    });
+    res.json(result);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/graphql`);
+});
+
+module.exports = app;
